2909afbad081e1f41faebe34408a3e0ada580afd
admin <admin@example.com>
2023-04-27 05:04:23 +0100

    Appdrawer

--- lib/main.dart
+++ lib/main.dart
@@ -9,10 +9,24 @@ class Application extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const FluentApp(
+    return FluentApp(
       home: NavigationView(
         appBar: NavigationAppBar(title: Text("Hello world")),
-        content: Text("This is the content"),
+        // content: Text("This is the content"),
+        pane: NavigationPane(items: [
+          PaneItem(
+              icon: const Icon(FluentIcons.home),
+              body: SizedBox.shrink(),
+              title: Text("Home")),
+          PaneItem(
+              icon: const Icon(FluentIcons.user_event),
+              body: SizedBox.shrink(),
+              title: Text("Profile")),
+          PaneItem(
+              icon: const Icon(FluentIcons.settings),
+              body: SizedBox.shrink(),
+              title: Text("Settings"))
+        ]),
       ),
     );
   }
